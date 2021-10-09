d6a6a6cc220c91b8dc6ef4bcc5966dc073c4970b
ZeldoR <zeldor@ya.ru>
2021-10-09 20:54:44 +0300

    Fixed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,22 @@
 $(document).ready(function(){
 
+    // Display list
     $('.city-change__act-city').click(function(){
-        // Показываем / Прячем список
+        // Display / hide list
         $('.city-change__list').slideToggle(300);
     })
 
+    // Select new city
     $('.city-change__list-item').click(function(){
-        // Убираем активный класс у всех пунктов
+        // Remove active class from all items
         $('.city-change__list-item').each(function(){
             $(this).removeClass('city-change__list-item_active');
         });
-        // Добавляем активный класс выбранному
+        // Add active class to selected item
         $(this).addClass('city-change__list-item_active');
-        // Прячем список
+        // Hide list
         $('.city-change__list').slideToggle(300);
-        // Выводим выбранный город
+        // Change select city
         $cityName = $(this).text();        
         $('.city-change__act-city').text($cityName);
     })
